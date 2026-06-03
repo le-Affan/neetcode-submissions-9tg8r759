75cbe94fac9de148ef94665e1f924d8c5f29e1cc
Affan <shaikhaffan1234@gmail.com>
2026-06-03 14:44:27 +0530

    Add: reverse-linked-list-ii - submission-0

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-0.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        tail.next = head
+
+        curr = tail
+        count = 0
+
+        while count <= right:
+            if count < left:
+
+                if count == left - 1:
+                    preList, prev = curr, curr
+                
+                    curr = curr.next
+                    count += 1
+                    continue
+
+                curr = curr.next
+                count += 1
+                continue
+            
+            if count == left:
+                endList = curr
+
+            if left <= count:      
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+                
+                count += 1
+        
+        endList.next = curr
+        preList.next = prev
+
+        return dummy.next
+
+
+
+
+
